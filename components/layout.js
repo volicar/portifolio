2682b5582e6de9e4a119ec1331ca2385d9641b28
Vinicius Olicar <developer@MacBook-Pro.local>
2021-03-29 03:11:50 -0300

    colocando no google

--- components/layout.js
+++ components/layout.js
@@ -23,6 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} /> */}
+        <meta name="google-site-verification" content="fmaxZNtRNo6-nsK_SwSVEguXc25fn-HSN1vzL0z_HQ4" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
